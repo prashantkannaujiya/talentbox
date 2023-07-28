9e18b39fb0343953c575ef0a5404dab9cfab1b51
prashantkannaujiya <prashantkannaujiya@gmail.com>
2023-07-28 15:44:10 +0530

    Assignment

--- src/Dash.js
+++ src/Dash.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import './style.css';
 function Dash(props)
 {
     var [courses,setcourses]=useState([{courseName:'',pic:''}])
@@ -9,18 +9,18 @@ fetch('http://localhost:2100/fetchCourse')
 .then(res=>res.json())
 .then((data)=>{
     console.log(data)
-setcourses(data);
+setcourses(data[0].courses);
 })
     },[])
     return(
-        <div>
+        <div id='Dashh'>
 <h4>Welcome to freeCodeCamp.org</h4>
 <p>I have not failed. I've just found 10,000 ways that won't work</p>
 <p>Thomas A. Edison</p>
-<div>
+<div id='dash'>
     {
 courses.map((a)=>{
-    return <div>
+    return <div id='courseBox'>
     <img src={a.pic}/>
     <span>{a.courseName}</span>
     </div>
